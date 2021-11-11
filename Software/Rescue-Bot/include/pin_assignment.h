fa12e4b586444d2aed895c23b1c843a2d3352822
Abhinav Agrahari <abhiraja1012@gmail.com>
2021-11-11 16:07:15 -0500

    Add startup buzzer

---
+++ Software/Rescue-Bot/include/pin_assignment.h
@@ -0,0 +1,26 @@
+#ifndef PIN_ASSIGNMENT_H
+#define PIN_ASSIGNMENT_H
+
+#include <Arduino.h>
+
+/*
+    Pin Assignments  
+*/
+// #define SCL         D15 // already defined in Arduino.h
+// #define SDA         D14 // already defined in Arduino.h
+//TODO: find left vs right motor. 
+#define ENA_PWM     D12
+#define IN1         D11
+#define IN2         D10
+#define IN3         D9
+#define IN4         D8
+#define ENB_PWM     D7
+#define SERVO_PWM   D6
+#define ENC1_A      D5
+#define ENC1_B      D4
+#define ENC2_A      D3
+#define ENC2_B      D2
+#define IMU_INT     A0
+#define BUZZER      A1
+
+#endif
--- Software/Rescue-Bot/src/main.cpp
+++ Software/Rescue-Bot/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h> // I2C
-#include <SPI.h> // Required for Color Sensor library to compile
+#include <SPI.h>  // Required for Color Sensor library to compile
 
 // TODO: Include task header files
 // TODO: Create global sensor/actuator objects
@@ -9,15 +9,28 @@
 // Task t_navigation
 // Task t_motorControl
 
+#include "pitches.h"
+#include "pin_assignment.h"
+
+void playStartupSound(){
+  pinMode(BUZZER,OUTPUT);
+  int dur = 1000/8;
+  float dur_between = dur * 1.3;
+  tone(BUZZER, NOTE_C5, dur);
+  delay(dur_between);
+  noTone(BUZZER);
+  tone(BUZZER, NOTE_E5, dur);
+  delay(dur_between);
+  noTone(BUZZER);
+  tone(BUZZER, NOTE_F5, dur);
+  delay(dur_between);
+  noTone(BUZZER);
+}
+
 void setup()
 {
+  playStartupSound();
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop()
