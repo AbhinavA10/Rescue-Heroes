e30133d0a344271181a9b02fd9b8f8a2d7bae109
Abhinav Agrahari <abhiraja1012@gmail.com>
2021-11-11 17:39:43 -0500

    Add basic color sensor and I2C Mux

--- Software/Rescue-Bot/src/main.cpp
+++ Software/Rescue-Bot/src/main.cpp
@@ -8,22 +8,26 @@
 // Task t_readSensors
 // Task t_navigation
 // Task t_motorControl
+#include "pin_assignment.h"
+#include "buzzer.h"
 
 #include "task/motor_control.h"
 #include "task/display.h"
+#include "task/read_sensors.h"
 
-#include "pin_assignment.h"
-#include "buzzer.h"
+
 void setup()
 {
   playStartupSound();
   Serial.begin(115200);
   // for testing:
   // MotorControl::init_motor_control();
-  Display::test_display();
+  // Display::test_display();
+  init_sensors();
 }
 
 void loop()
 {
   //TODO: Run Task Scheduler
+  read_sensors(); // read color sensors
 }
--- Software/Rescue-Bot/src/task/read_sensors.cpp
+++ Software/Rescue-Bot/src/task/read_sensors.cpp
@@ -9,24 +9,73 @@ void init_sensors()
 {
     // Start I2C bus
     Wire.begin();
-    imu->init();
-    imu->run();
-    read_sensors();
+    // imu->init();
+    // imu->run();
+    // read_sensors();
     // The next runs of this task will use the read_sensors callback
-    t_readSensors.setCallback(&read_sensors);
+    // t_readSensors.setCallback(&read_sensors);
+    setup_colors();
 }
 
+void chooseBus(uint8_t bus)
+{
+    Wire.beginTransmission(0x70);
+    Wire.write(1 << (bus + 2)); // will be using 2-3 instead of 0-1 SDA/SCL pins because of convience (placed better on the breadboard)
+    Wire.endTransmission();
+}
+
+void setup_colors()
+{
+    for (int i = 0; i < numberOfSensors; i++)
+    {
+        Serial.println(i);
+        chooseBus(i);
+        if (tcs[i].begin())
+        {
+            Serial.print("Found sensor ");
+            Serial.println(i + 1);
+        }
+        else
+        {
+            Serial.println("No Sensor Found");
+            while (true)
+                ;
+        }
+    }
+    delay(2000);
+    Serial.println("start");
+}
 void read_sensors()
 {
-    if (MotorControl::current_command.type == Command_t::TURN)
-    {
-        // TODO: only run if we're still turning?
-        imu->readData();
-    }
+    // if (MotorControl::current_command.type == Command_t::TURN)
+    // {
+    // TODO: only run if we're still turning?
+    // imu->readData();
+    // }
     //TODO: set bus using Mux for each color sensor
     //detectColor();
+    for (int i = 0; i < numberOfSensors; i++)
+    {
+        chooseBus(i);
+        float red, green, blue;
+        tcs[i].getRGB(&red, &green, &blue);
+
+        // Comment out this section below if wanting to use with colorviewCollectorPDE!
+        Serial.print("Sensor: ");
+        Serial.print(i);
+        Serial.print("\t");
+
+        Serial.print("R:\t");
+        Serial.print(int(red));
+        Serial.print("\tG:\t");
+        Serial.print(int(green));
+        Serial.print("\tB:\t");
+        Serial.print(int(blue));
+        Serial.print("\n");
+    }
+    delay(60); // takes 50ms to read
 
     // update encoder distance if needed
-    motors.left->readDistance();
-    motors.right->readDistance();
+    // motors.left->readDistance();
+    // motors.right->readDistance();
 }
