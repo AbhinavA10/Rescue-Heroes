09e9516893d0f601b0efe95f90802f1c5db44667
Abhinav Agrahari <abhiraja1012@gmail.com>
2021-11-11 17:19:09 -0500

    Test OLED display

---
+++ Software/Rescue-Bot/src/task/display.h
@@ -0,0 +1,23 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <Arduino.h>
+#include "config.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// extern Task t_Display;
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C /// Normally 0x3D is for 128x64 but our's has this address.
+namespace Display
+{
+    static Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+    void test_display();
+};
+
+#endif
