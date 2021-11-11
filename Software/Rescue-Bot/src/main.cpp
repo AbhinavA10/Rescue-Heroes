fe9fb023c6f6b0984b213dc506b6b58c91621770
liamc2042 <liam_clark2042@hotmail.com>
2021-11-11 17:44:40 -0500

    Added Tasks and Callbacks

--- Software/Rescue-Bot/src/main.cpp
+++ Software/Rescue-Bot/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h> // I2C
 #include <SPI.h>  // Required for Color Sensor library to compile
+#include <TaskScheduler.h>
 
 // TODO: Include task header files
 // TODO: Create global sensor/actuator objects
@@ -11,15 +12,27 @@
 
 #include "task/motor_control.h"
 #include "task/display.h"
+#include "task/navigation.h"
+#include "task/read_sensors.h"
 
 #include "pin_assignment.h"
 #include "buzzer.h"
+
+void sensorCallback();
+void navigationCallback();
+void motorCallback();
+
+Task t_readSensors(1000, 5, &sensorCallback);
+Task t_navigation(1000, 5, &navigationCallback);
+Task t_motorControl(1000, 5, &motorCallback);
+
+
 void setup()
 {
   playStartupSound();
   Serial.begin(115200);
   // for testing:
-  // MotorControl::init_motor_control();
+  MotorControl::init_motor_control();
   Display::test_display();
 }
 
