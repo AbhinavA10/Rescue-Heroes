0fffc47a96044224f13c45056465abb9ff892f52
Abhinav Agrahari <abhiraja1012@gmail.com>
2021-11-13 16:40:03 -0500

    Update OLED with Sensor init statuses

--- Software/Rescue-Bot/src/task/display.h
+++ Software/Rescue-Bot/src/task/display.h
@@ -11,18 +11,26 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C /// Normally 0x3D is for 128x64 but our's has this address.
+#define OLED_RESET -1            // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_SCREEN_ADDRESS 0x3C /// Normally 0x3D is for 128x64 but our's has this address.
+
+// Data positions in string array
+#define COLOR_SENSOR_STATUS_INDEX 0
+#define IMU_SENSOR_STATUS_INDEX 1
+#define YAW_INDEX 2
+#define COLOR_CLASS_INDEX 3
+
 namespace Display
 {
-    static Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+    extern Adafruit_SSD1306 display;
+    extern String text[8];
 
+    void init_display();
+    void refresh_display();
     void test_display();
     void testdrawchar();
     void testdrawstyles();
-    void init_display();
-    void display_text(String text);
-    
+
 };
 
 #endif
