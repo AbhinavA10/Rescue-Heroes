afe0f9cb8cac5e4f1e6a483d402a2fe5f3ef52c7
Daniel Leung <daniel.leung@uwaterloo.ca>
2021-11-15 16:43:13 -0700

    psuedo code logic for navigation

--- Software/Rescue-Bot/src/task/navigation.cpp
+++ Software/Rescue-Bot/src/task/navigation.cpp
@@ -20,7 +20,7 @@ namespace Navigation
             do_find_lego_man();
             break;
         case State_t::FOUND_LEGO_MAN:
-            do_pick_up_lego_man();   
+            do_pick_up_lego_man();
             break;
         case State_t::FINDING_SAFE_ZONE:
             do_finding_safe_zone();
@@ -29,7 +29,8 @@ namespace Navigation
             do_dropoff_lego_man();
             break;
         case State_t::RETURN_TO_START:
-            do_follow_red_line();
+            // do_follow_red_line();
+            do_return_to_start();
             break;
         case State_t::TEST_MOVE:
             do_test_move();
@@ -38,21 +39,119 @@ namespace Navigation
             break;
         };
     }
-    void do_follow_red_line()
-    {
-        //TODO
-    }
+
+    // void do_follow_red_line()
+    // {
+
+    // }
 
     void do_find_lego_man()
     {
-        do_follow_red_line();
-        //TODO
+        // do_follow_red_line(); // commenting out for now. can implement if needed.
+
+        while (state == State_t::FINDING_LEGO_MAN)
+        {
+            // read_sensors();
+            // if (!read_red()) // the sensor does not read red, drive forward
+            // {
+            //     MotorControl::drive_fwd();
+            // }
+            // else if (read_red()) // sees red
+            // {
+            //     if (left_sensor_red())
+            //     {
+            //         while (left_sensor_red)
+            //         {
+            //             // MotorControl::spin_right();
+            //             // increase right motor speed
+            //         }
+            //     }
+            //     else if (right_sensor_red())
+            //     {
+            //         while (right_sernsor_red())
+            //         {
+            //             // MotorControl::spin_left();
+            //             // increase left motor speed
+            //         }
+            //     }
+            // }
+            // else if (read_blue()) // sees blue
+            // {
+            //     state = State_t::FOUND_LEGO_MAN;
+            // }
+        }
     }
 
     void do_finding_safe_zone()
     {
-        do_follow_red_line();
-        //TODO
+        // do_follow_red_line();
+        while (state == State_t::FINDING_SAFE_ZONE)
+        {
+            // read_sensors();
+            // if (!read_red()) // the sensor does not read red, drive forward
+            // {
+            //     MotorControl::drive_fwd();
+            // }
+            // else if (read_red()) // sees red
+            // {
+            //     if (left_sensor_red())
+            //     {
+            //         while (left_sensor_red)
+            //         {
+            //             // MotorControl::spin_right();
+            //             // increase right motor speed
+            //         }
+            //     }
+            //     else if (right_sensor_red())
+            //     {
+            //         while (right_sernsor_red())
+            //         {
+            //             // MotorControl::spin_left();
+            //             // increase left motor speed
+            //         }
+            //     }
+            // }
+            // else if (read_green()) // sees blue
+            // {
+            //     state = State_t::FOUND_SAFE_ZONE;
+            // }
+        }
+    }
+
+    void do_return_to_start()
+    {
+        // do_follow_red_line();
+        while (state == State_t::RETURN_TO_START)
+        {
+            // read_sensors();
+            // if (!read_red()) // the sensor does not read red, drive forward
+            // {
+            //     MotorControl::drive_fwd();
+            // }
+            // else if (read_red()) // sees red
+            // {
+            //     if (left_sensor_red())
+            //     {
+            //         while (left_sensor_red)
+            //         {
+            //             // MotorControl::spin_right();
+            //             // increase right motor speed
+            //         }
+            //     }
+            //     else if (right_sensor_red())
+            //     {
+            //         while (right_sernsor_red())
+            //         {
+            //             // MotorControl::spin_left();
+            //             // increase left motor speed
+            //         }
+            //     }
+            // }
+            // else if (read_green()) // sees blue
+            // {
+            //     state = NULL;
+            // }
+        }
     }
 
     void do_pick_up_lego_man()
